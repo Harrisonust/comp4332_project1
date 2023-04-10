256e4eb80825834eeb0446388652718e8c60ad45
Harrison <sloab@ust.hk>
2023-04-11 02:02:53 +0800

    checkpoint

--- utils.py
+++ utils.py
@@ -27,28 +27,7 @@ def load_data(split_name='train', columns=['text', 'stars'], folder='./../data')
         print(f"Failed loading specified columns... Returning all columns from the {split_name} split")
         df = pd.read_csv(f'{folder}/{split_name}.csv')
         return df
-def load_preprocess(split_name='train', columns=['text', 'stars'], folder='./../data',embedding='sentence'):
-    
-    if embedding=='sentence':
-        df=pd.read_csv(f'{folder}/{split_name}_{embedding}.csv',converters=dict(sent_embed=literal_eval))
-        if split_name!="test":
-            df = df.loc[:,columns+['sent_embed']]
-    elif embedding=='word':
-        print(f'Reading {split_name} csv file...')
-        df=pd.read_csv(f'{folder}/{split_name}_{embedding}.csv')
-        if split_name!="test":
-            df = df.loc[:,columns+['word_embed']]
-    elif embedding=='subtext':
-        print(f'Reading {split_name} csv file...')
-        df=pd.read_csv(f'{folder}/{split_name}_{embedding}.csv')
-        if split_name!="test":
-            df = df.loc[:,columns+['subtext_embed']]
-    else:
-        print(f'Reading {split_name} csv file...')
-        df=pd.read_csv(f'{folder}/{split_name}_{embedding}.csv')
-        if split_name!="test":
-            df = df.loc[:,columns+['subsentence_embed']]
-    return df
+
 def lower(s):
     """
     :param s: a string.
@@ -142,63 +121,3 @@ def get_onehot_vector(feats, feats_dict):
             # set the corresponding element as 1
             vector[f_idx] = 1
     return vector
-
-class Embedder:
-    def __init__(self,embedder,tokenizer):
-        self.embedder=spacy.load(embedder)
-        self.tokenizer=tokenizer
-    def word_embedding(self,text):
-        tokens = lower(filter_stopwords(tokenize(text)))
-        result = np.zeros((128,96))
-        i=0
-        for token in tokens:
-            if i<64:
-                result[i]=(self.embedder(token).vector.tolist())
-            else:
-                break
-            i+=1
-        return result.tolist()
-    def sentence_embedding(self,text):
-        return self.embedder(text).vector.tolist()
-    def subsentence_embedding(self,text):
-        tokens = tokenize(text)
-        sublen = len(tokens)//10
-        subsentences = [' '.join(tokens[i*sublen:(i+1)*sublen]) for i in range(9)]
-        subsentences.append(' '.join(tokens[(9)*sublen:]))
-        result = np.zeros((10,96))
-        for i in range(10):
-            v=(self.embedder(subsentences[i]).vector.tolist())
-            if len(v)==0:
-                v=[0]*96
-            result[i]= v
-        return result.tolist()
-    def subtext_embedding(self,text):
-        sentences = self.tokenizer.tokenize(text)
-        result = np.zeros((10,96))
-        i=0
-        for s in sentences:
-            if i<10:
-                result[i]=(self.embedder(s).vector.tolist())
-            else:
-                break
-            i+=1
-        return result.tolist()
-
-
-def load_cfg(cfg):
-    hyp = None
-    if isinstance(cfg, str):
-        with open(cfg, errors='ignore') as f:
-            hyp = yaml.safe_load(f)  # load hyps dict
-    return Namespace(**hyp)
-
-def merge_args_cfg(args, cfg):
-    dict0 = vars(args)
-    dict1 = vars(cfg)
-    dict = {**dict0, **dict1}
-
-    return Namespace(**dict)
-
-def write_to_csv(file,df):
-    df.to_csv(file)
-
